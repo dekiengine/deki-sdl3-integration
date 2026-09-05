28a99dd1a0ac209f235880c4f638aab2a2383a7e
Kirbyrawr <devkirbyrawr@gmail.com>
2026-09-05 10:15:23 +0200

    Docs for deki-sdl3-integration from deki-editor 185b802

---
+++ navtreeindex0.js
@@ -0,0 +1,10 @@
+var NAVTREEINDEX0 =
+{
+"index.html":[],
+"index.html#autotoc_md0":[0],
+"index.html#autotoc_md1":[1],
+"index.html#autotoc_md2":[2],
+"index.html#c-sdl3displaysetup":[2,0],
+"index.html#c-sdl3inputsetup":[2,1],
+"pages.html":[]
+};
